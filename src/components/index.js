ecf9e5a635d9c8947556273103d35da17290013f
Taimoor khan <khant8k83@gmail.com>
2020-11-18 17:11:22 +0500

    commit: folder structure setup | added module export

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+/* 
+for exporting modules and accessing them easily as: 
+import { Cards, CountryPicker, Chart } from './components';
+*/
+export { default as Chart } from "./Chart/Chart";
+export { default as Cards } from "./Cards/Cards";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
